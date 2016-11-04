6f87a6ade3173fa061a3b1953a83a771a46e7d25
TheCmar7 <runner.cram@gmail.com>
2016-11-04 13:48:40 -0700

    Added a bunch of comment organization and added input validation to most of the functions

--- logit.py
+++ logit.py
@@ -17,10 +17,12 @@ from datetime import date, timedelta
 import os
 import readline
 
+# ******************************************************************************
 #	tryGettingFile
 #
 #	This will try to connect to the file and then return the file name or kill the
 #	program
+# ******************************************************************************
 def tryGettingFile():
 	try:
 		currentLogName = pickle.load( open(filePath + infoFile, "rb") )['current']
@@ -31,7 +33,9 @@ def tryGettingFile():
 	# if it succeeds
 	return currentLogName;
 
+# ******************************************************************************
 # so I can change the output colors
+# ******************************************************************************
 class bcolors:
 	HEADER = '\033[95m'
 	OKBLUE = '\033[94m'
@@ -42,7 +46,10 @@ class bcolors:
 	BOLD = '\033[1m'
 	UNDERLINE = '\033[4m'
 
-#complete = "/usr/local/my_scripts"
+# ******************************************************************************
+# --> GLOBALS <--
+
+# complete = "/usr/local/my_scripts"
 # these need to become variables in the info file.
 complete = "/Users/TheCmar7/Developer/python/Logit"
 directory = "/logit_saves/"
@@ -51,96 +58,129 @@ filePath = complete + directory
 
 cmds = {}
 currentLogName = tryGettingFile()
+# ******************************************************************************
 
+
+# ******************************************************************************
 # 	helpText():
 #
-#	Prints out the text of  commands
+#	Prints out the text of commands
+# ******************************************************************************
 def helpText():
 	for k, v in cmds.items():
 		print( "\t" + k + " " + v.desc)
 
-
+# ******************************************************************************
+#	isALog ( log )
+#
+#	Returns true if the log is one of the saved logs.
+# ******************************************************************************
 def isALog( log ):
-	return log in pickle.load( open(file + infoFile, "rb" ))['logs']
+	return log in pickle.load( open(filePath + infoFile, "rb" ))['logs']
 
-
+# ******************************************************************************
 # 	setCurrentLog ( name ):
 #
-# 	This sets the current log to a name 
+# 	This sets the current log to a name
+# ******************************************************************************
 def setCurrentLog( name ):
-	workingLog = pickle.load( open(filePath + infoFile, "rb" ))
-	workingLog['current'] = name
-	pickle.dump( workingLog, open(filePath + infoFile, "w"))
+	if isALog(name):
+		workingLog = pickle.load( open(filePath + infoFile, "rb" ))
+		workingLog['current'] = name
+		pickle.dump( workingLog, open(filePath + infoFile, "w"))
+		return True
+	else:
+		errorWarning(name + " is not a log")
+		return False
 
-
+# ******************************************************************************
 #	newLog ( name ):
 #
-#	creates a new log with the name that is given 
+#	creates a new log with the name that is given
+# ******************************************************************************
 def newLog( name ):
-	if isALog( name ):
-		errorWarning( name + " is already a log!" )
-		return 0
-	
-	workingLog = pickle.load( open(filePath + infoFile, "rb" ))
-	workingLog['logs'].append(name)
-	workingLog['count'] = workingLog['count'] + 1;
-	pickle.dump (workingLog, open(filePath + infoFile, "wb"))
-
-	print("created a new log: " + sys.argv[2])
-	now = datetime.datetime.now()
-	newLog = {
-				"name": name,
-				"dateCreated": now.strftime("%Y-%m-%d %H:%M"),
-				"lastEdit": now.strftime("%Y-%m-%d %H:%M"), 
+	if not isALog( name ):
+		workingLog = pickle.load( open(filePath + infoFile, "rb" ))
+		workingLog['logs'].append(name)
+		workingLog['count'] = workingLog['count'] + 1;
+		pickle.dump (workingLog, open(filePath + infoFile, "wb"))
+		
+		print("created a new log: " + sys.argv[2])
+		now = datetime.datetime.now()
+		newLog = {
+			"name": name,
+			"dateCreated": now.strftime("%Y-%m-%d %H:%M"),
+				"lastEdit": now.strftime("%Y-%m-%d %H:%M"),
 				"count": 0,
-			 	"logs":	[]
+				"logs":	[]
 			 }
 
-	pickle.dump( newLog, open(filePath + name, "wb") )
-	setCurrentLog(name)
+		pickle.dump( newLog, open(filePath + name, "wb") )
+		setCurrentLog(name)
+		return True
+	else:
+		errorWarning( name + " is already a log" )
+		return False
 
 
-#	logTo ( log, instr):
+
+# ******************************************************************************
+#	logTo ( instring, log = currentLog ):
 #
-#	logs the given message to the log 
+#	logs the given message to the log
+# ******************************************************************************
 def logTo( instring, log = currentLogName ):
-	# load log
-	workingLog = pickle.load( open(filePath + log, "rb" ))
+	if isALog(log):
+		
+		workingLog = pickle.load( open(filePath + log, "rb" ))
 
-	# TODO: get the date thingy to working
-	# if last edit is one day ago
-	now = datetime.datetime.now()
-	
-	t = time.strptime(workingLog['lastEdit'], "%Y-%m-%d %H:%M")
-	nextDate = datetime.datetime(t.tm_year, t.tm_mon, t.tm_mday) + timedelta(1)
+		# TODO: get the date thingy to working
+		# if last edit is one day ago
+		now = datetime.datetime.now()
+		
+		t = time.strptime(workingLog['lastEdit'], "%Y-%m-%d %H:%M")
+		nextDate = datetime.datetime(t.tm_year, t.tm_mon, t.tm_mday) + timedelta(1)
 
-	if ( nextDate < now ):
-		putDateInLog( log ) 
+		if ( nextDate < now ):
+			putDateInLog( log ) 
 
-	mes = str(now.strftime("%H:%M - ")) + instring
+		mes = str(now.strftime("%H:%M - ")) + instring
 
-	workingLog['logs'].append(mes)
-	workingLog['lastEdit'] = now.strftime("%Y-%m-%d %H:%M")
-	workingLog['count'] = workingLog['count'] + 1
-	pickle.dump (workingLog, open(filePath + log, "w"))
+		workingLog['logs'].append(mes)
+		workingLog['lastEdit'] = now.strftime("%Y-%m-%d %H:%M")
+		workingLog['count'] = workingLog['count'] + 1
+		pickle.dump (workingLog, open(filePath + log, "w"))
+		return True
+	else:
+		errorWarning(log + "is not a log")
+		return False
 
 
-# 	printOutLog( log ):
+# ******************************************************************************
+# 	printOutLog( log = currentLog ):
 #
 # 	Prints out all the saved strings that are in a log
+# ******************************************************************************
 def printOutLog( log = currentLogName ):
-	workingLog = pickle.load( open(filePath + log, "rb" ))['logs']
-	for log in workingLog:
-		if (log[0] == '['):
-			print(log)
-		elif (log[0] == "|"):
-			print("      " + log)
-		else:
-			print("    " + log)
+	if isALog(log):
+		workingLog = pickle.load( open(filePath + log, "rb" ))['logs']
+		for log in workingLog:
+			if (log[0] == '['):
+				print(log)
+			elif (log[0] == "|"):
+				print("      " + log)
+			else:
+				print("    " + log)
+		return True
+	else:
+		errorWarning(log + " is not a log")
+		return False
 
-#	putDateInLog( log ):
+# ******************************************************************************
+#	putDateInLog( log = currentLog ):
 #
 #	This adds a log that is the current date 
+# ******************************************************************************
 def putDateInLog( log = currentLogName):
 	now = datetime.datetime.now()
 	workingLog = pickle.load( open(filePath + log, "rb" ))
@@ -148,64 +188,88 @@ def putDateInLog( log = currentLogName):
 	workingLog['lastEdit'] = now.strftime("%Y-%m-%d %H:%M")
 	pickle.dump(workingLog, open(filePath + log, "w"))
 
-#	outInfo( log ):
+# ******************************************************************************
+#	outInfo( log = currentLog ):
 #
 #	Prints out the info of the of the log
+# ******************************************************************************
 def outInfo( log = currentLogName ):
-	workingLog = pickle.load( open(filePath + log, "rb" ))
-	print("name:		" + workingLog['name'])
-	print("Created: 	" + workingLog['dateCreated'])
-	print("Last Editted:	" + workingLog['lastEdit'])
-	print("# of logs:   	" + str(workingLog['count']))
+	if isALog(log):
+		workingLog = pickle.load( open(filePath + log, "rb" ))
+		print("name:		" + workingLog['name'])
+		print("Created: 	" + workingLog['dateCreated'])
+		print("Last Editted:	" + workingLog['lastEdit'])
+		print("# of logs:   	" + str(workingLog['count']))
+		return True
+	else:
+		errorWarning(log + " is not a log")
+		return False
 
+# ******************************************************************************
 # 	listOutLogs():
 #
 #	This prints out the all of the current notes 
+# ******************************************************************************
 def listOutLogs():
 	savedLogs = pickle.load( open(filePath + infoFile, "rb" ))['logs']
 	for log in savedLogs:
 		print("\t" + log)
 
+# ******************************************************************************
 #	saveLogToFile():
 #
 #	This saves the log to a text file 
+# ******************************************************************************
 def saveLogToFile( file, log = currentLogName ):
-	workingLog = pickle.load( open( filePath + log, "rb" ) )
-	f = open( file, 'w' )
-	f.write( "name:     \t" + workingLog["name"] + "\n" )
-	f.write( "Created:  \t" + workingLog["dateCreated"] + "\n" )
-	f.write( "Edited:   \t" + workingLog["lastEdit"] + "\n" )
-	f.write( "Log Count:\t" + str( workingLog["count"] ) + "\n" )
-	f.write( "-- Log -- \n" )
-	logs = workingLog['logs']
-	for l in logs:
-		f.write(l + "\n")
+	if isALog(log):
+		workingLog = pickle.load( open( filePath + log, "rb" ) )
+		f = open( file, 'w' )
+		f.write( "name:     \t" + workingLog["name"] + "\n" )
+		f.write( "Created:  \t" + workingLog["dateCreated"] + "\n" )
+		f.write( "Edited:   \t" + workingLog["lastEdit"] + "\n" )
+		f.write( "Log Count:\t" + str( workingLog["count"] ) + "\n" )
+		f.write( "-- Log -- \n" )
+		logs = workingLog['logs']
+		for l in logs:
+			f.write(l + "\n")
+		return True
+	else:
+		errorWarning(log + " is not a log")
+		return False
 
 
 
-# 	starting ( log ):
+# ******************************************************************************
+# 	starting ( log = currentLog ):
 # 
 #	Allows the user to input consecutive logs
+# ******************************************************************************
 def starting( log = currentLogName ):
-	os.system('clear')
-	inputs = ""
-	print("\"-quit\" to stop logging\n\"-d\" to date")
+	if isALog(log):
+		os.system('clear')
+		inputs = ""
+		print("\"-quit\" to stop logging\n\"-d\" to date")
 
-	# Keeps taking input until the user specifies quit.
-	while (inputs != "-quit"):
-		inputs = raw_input(log + bcolors.OKBLUE + " >> " + bcolors.ENDC)
-		if (inputs != "-quit"):
-			if (inputs == "-d"):
-				putDateInLog(log)
-			else:	
-				logTo(log, inputs)
+		# Keeps taking input until the user specifies quit.
+		while (inputs != "-quit"):
+			inputs = raw_input(log + bcolors.OKBLUE + " >> " + bcolors.ENDC)
+			if (inputs != "-quit"):
+				if (inputs == "-d"):
+					putDateInLog(log)
+				else:	
+					logTo(log, inputs)
+		return True
+	else:
+		errorWarning(log + " is not a log")
+		return False
 
 
-
+# ******************************************************************************
 #	tryGettingFile
 #
 #	This will try to connect to the file and then return the file name or kill the
 #	program
+# ******************************************************************************
 def tryGettingFile():
 	try:
 		currentLogName = pickle.load( open(filePath + infoFile, "rb") )['current']
@@ -216,46 +280,62 @@ def tryGettingFile():
 	# if it succeeds 
 	return currentLogName;
 
-#   EditLog ( log ):
+# ******************************************************************************
+#   EditLog ( log = currentLog ):
 #
 # 	Will open the log and allow the user to edit the log
+# ******************************************************************************
 def editLog( log = currentLogName ):
-	workingLog = pickle.load( open( filePath + log, "rb" ) )
-	for i in range(0, len(workingLog["logs"])):
-		print("[" + str(i + 1) + "] " + workingLog["logs"][i] )
+	if (isALog(log)):
+		workingLog = pickle.load( open( filePath + log, "rb" ) )
+		for i in range(0, len(workingLog["logs"])):
+			print("[" + str(i + 1) + "] " + workingLog["logs"][i] )
 
-	theI = raw_input("# to delete (q to quit): ")
+		theI = raw_input("# to delete (q to quit): ")
 
-	# Q to quit 
-	if (theI == 'q'):
-		return 0
+		# Q to quit 
+		if (theI == 'q'):
+			return 0
 
-	# Checking the edit input
-	if (int(theI) - 1 >= len(workingLog['logs']) or int(theI) <= 0):
-		print("not in index")
-		return 0
+		# Checking the edit input
+		if (int(theI) - 1 >= len(workingLog['logs']) or int(theI) <= 0):
+			print("not in index")
+			return 0
 
-	ans = raw_input("Delete \'" + workingLog['logs'][int(theI) - 1] + "\'? Y/n ")
-	if (ans.upper()[0] == 'Y'): 
-		workingLog['logs'].pop(int(theI) - 1)
-		print("Deleted")
-		workingLog['count'] = workingLog['count'] - 1
+		ans = raw_input("Delete \'" + workingLog['logs'][int(theI) - 1] + "\'? Y/n ")
+		if (ans.upper()[0] == 'Y'): 
+			workingLog['logs'].pop(int(theI) - 1)
+			print("Deleted")
+			workingLog['count'] = workingLog['count'] - 1
 
-	pickle.dump(workingLog, open(filePath + log, "w"))
+		pickle.dump(workingLog, open(filePath + log, "w"))
+		return True
+	else:
+		errorWarning(log + "is not a log")
+		return False
 
-#	Breaking
+# ******************************************************************************
+#	Breaking ( log = currentLog )
 #
 #	Adds a line to the timeline making the log look a little nicer.
+# ******************************************************************************
 def breakLine( log = currentLogName ):
-	print("Break Line Added")
-	workingLog = pickle.load( open( filePath + log, "rb" ) )
-	workingLog['logs'].append("|")
-	pickle.dump( workingLog, open( filePath + log, "w" ) )
+	if (isALog(log)):
+		print("Break Line Added")
+		workingLog = pickle.load( open( filePath + log, "rb" ) )
+		workingLog['logs'].append("|")
+		pickle.dump( workingLog, open( filePath + log, "w" ) )
+		return True
+	else:
+		errorWarning(log + " is not a log")
+		return False
 
+# ******************************************************************************
 #	inputLogFromFile
 #
 #	This will allow the user to save from a file that was outputted from his
 #	program
+# ******************************************************************************
 def inputLogFromFile( file ):
 	
 	# Try to open the file
@@ -291,14 +371,18 @@ def inputLogFromFile( file ):
 		# save the new log
 		pickle.dump( workingLog, open( filePath + name, "w" ) )
 
+		return True
 	# file not a thing
 	except IOError:
-		print( file + ": Is not a file.")
+		errorWarning( file + ": Is not a file.")
+		return False
 
+# ******************************************************************************
 #	setup
 #
 #	This function is used to initially set up the info file of the current
 #	log and what not
+# ******************************************************************************
 def setup():
 	current = {
 		"current": "",
@@ -320,61 +404,74 @@ def setup():
 			pickle.dump( current, open(filePath + infoFile, "w") )
 			currentLogName = pickle.load( open( filePath + infoFile, "rb" ) )['current']
 
-#	deleteLog( log )
+# ******************************************************************************
+#	deleteLog( log = currentLog )
 #
 #	Will allow the user to delete an entire log
+# ******************************************************************************
 def deleteLog( log = currentLogName ):
-	workingInfo = pickle.load( open( filePath + infoFile, "rb" ))
-	if ( log in workingInfo['logs'] ):
+	if (isALog(log)):
+		workingInfo = pickle.load( open( filePath + infoFile, "rb" ))
 		ans = raw_input("Are you sure you want to delete \'" + log + "\'? Y/n ")
 		if (ans.upper()[0] == 'Y'):
 			workingInfo['logs'].remove(log)
 			pickle.dump( workingInfo, open(filePath + infoFile, "w") )
 			os.remove(filePath + log)
 			#TODO change current
+		return True
 	else:
+		# Print Warning, Return False
 		print("not a log")
+		return False
 
 
 
-# TODO:	longestLog( log )
+# ******************************************************************************
+# TODO:	longestLog( log = currentLog )
 #
 #	getting the longest log from a given log
+# ******************************************************************************
 def longestLog( log = currentLogName ):
-	workingLog = pickle.load( open( filePath + log, rb ) )
-	len = 0
-	ind = 0
-	for log in workingLog['logs']:
-		if ( len > len(log) ):
-			len = len(log)
+	if (isALog(log)):
+		workingLog = pickle.load( open( filePath + log, rb ) )
+		len = 0
+		ind = 0
+		for log in workingLog['logs']:
+			if ( len > len(log) ):
+				len = len(log)
 
-	return len
+		return len
+	else:
+		return False
 
-# TODO:	shortenString( input, len )
+# ******************************************************************************
+#	shortenString( input, len )
 #
 #	Shorten the string an returns an array of the given length
+# ******************************************************************************
 def shortenString( input, len ):
 	chunks = len(input)
 	chunk_size = len
 	return [ x[i:i+chunk_size] for i in range(0, chunks, chunk_size) ]
 
-# TODO: insert date
-#
-# ###
-
+# ******************************************************************************
 #	errorWarning( errorString )
 #
 #
+# ******************************************************************************
 def errorWarning( errorString ):
 	print(bcolors.WARNING + "ERROR: " + bcolors.ENDC + errorString)
 
+# ******************************************************************************
 #	outCurrent()
 #
 #	Prints out the current working log
+# ******************************************************************************
 def outCurrent():
 	print(tryGettingFile())
 
 
+# ******************************************************************************
 #	Class Command
 #
 #	name
@@ -382,6 +479,7 @@ def outCurrent():
 #	desc
 #	func
 #	TODO: options
+# ******************************************************************************
 class Command:
 	def __init__(self, name, desc, func, argN = 0, argP = 0):
 		self.name = name
@@ -393,7 +491,9 @@ class Command:
 	def doFunction(self, args):
 		self.func(*args)
 
+# ******************************************************************************
 # The command strings
+# ******************************************************************************
 cmds = {
 	'-new'		: Command("new", "\'name\' : creates a new log", newLog, 1, 1),
 	'-n'		: Command("name",  ": prints out the name of the current log", outCurrent, 0, 1),
@@ -413,9 +513,11 @@ cmds = {
 	'--help'	: Command("help", ": Shows the help message", helpText)
 	}
 
+# ******************************************************************************
 #	main()
 #
 #	This is a the main funciton of the progam
+# ******************************************************************************
 def main():
 	currentLogName = tryGettingFile()
 	if (len(sys.argv) == 1):
@@ -427,22 +529,31 @@ def main():
 		if command in cmds:
 			c = cmds[command]
 			#set up the options
-			options = []
+			funcArgs = []
 			
 			if (c.argN == c.argP):
 				for j in range(0, c.argN):
 					options.append( sys.argv[i + 1 + j] )
-				i = i + c.argN
-		
-			c.doFunction(options)
+				
+				if (c.doFunction(funcArgs))
+					i = i + c.argN
+				else:
+					exit(1)
+			else:
+				for j in range(c.argN, c.argP):
+	
+	
 		elif (command[0] != '-'):
 			cmds['-to'].doFunction([command, currentLogName])
 
 		else:
 			errorWarning(command + " is not a valid command")
+
 		i = i + 1
 
 
 
+# ******************************************************************************
 # this is where it all starts
+# ******************************************************************************
 main()
